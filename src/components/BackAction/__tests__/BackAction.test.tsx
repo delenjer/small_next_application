55f717554e0acd1442e22dd3e92cb6b329dcf8e6
stanislav.mokhyr <stanislav.mokhyr@idapgroup.com>
2023-11-13 15:05:28 +0200

    Add demo link.

--- src/components/BackAction/__tests__/BackAction.test.tsx
+++ src/components/BackAction/__tests__/BackAction.test.tsx
@@ -2,21 +2,16 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import {BackAction} from '@/components/BackAction/BackAction';
 
-
 describe('BackAction Component', () => {
   it('renders the component with the correct link', () => {
     render(<BackAction />);
 
-    // Use screen.getByRole to find the link by its role
     const linkElement = screen.getByRole('link', { name: /Go back/i });
 
-    // Assert that the link is rendered
     expect(linkElement).toBeInTheDocument();
 
-    // Make sure the href is set correctly on the Link component
     expect(linkElement).toHaveAttribute('href', '/');
 
-    // You can also add more specific assertions about the link, for example:
-    expect(linkElement).toHaveClass('back'); // assuming the correct class name
+    expect(linkElement).toHaveClass('back');
   });
 });
--- src/components/ShowMoreAction/__tests__/ShowMoreAction.test.tsx
+++ src/components/ShowMoreAction/__tests__/ShowMoreAction.test.tsx
@@ -1,8 +1,6 @@
-// ShowMoreAction.test.js
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import {ShowMoreAction} from '@/components/ShowMoreAction/ShowMoreAction';
-
 
 describe('ShowMoreAction Component', () => {
   it('renders the component with the correct link', () => {
@@ -10,16 +8,12 @@ describe('ShowMoreAction Component', () => {
 
     render(<ShowMoreAction id={mockId} />);
 
-    // Use screen.getByRole to find the link by its role
     const linkElement = screen.getByRole('link', { name: /Show details/i });
 
-    // Assert that the link is rendered
     expect(linkElement).toBeInTheDocument();
 
-    // Make sure the href is set correctly on the Link component
     expect(linkElement).toHaveAttribute('href', `/details/${mockId}`);
 
-    // You can also add more specific assertions about the link, for example:
-    expect(linkElement).toHaveClass('showMore'); // assuming the correct class name
+    expect(linkElement).toHaveClass('showMore');
   });
 });
